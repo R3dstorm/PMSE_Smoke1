341133ee2fa1a2dab8c50c17565974dafbcafb79
SteffenHolz <steffen@holzhauer-freiburg.de>
2018-12-11 21:24:20 +0100

    test code for notification

--- wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
@@ -1,6 +1,11 @@
 package de.uni_freiburg.iems.beatit;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
@@ -26,16 +31,31 @@ public class EcologicalMomentaryAssesmentActivity extends WearableActivity {
 
         final Button button = findViewById(R.id.pressButton);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
+        Intent intent = new Intent(this, EcologicalMomentaryAssesmentActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Action action = new NotificationCompat.Action.Builder(
+                R.drawable.preference_wrapped_icon, getString(R.string.hello_world), pendingIntent).build();
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentText(getString(R.string.hello_world))
+                .setContentTitle(getString(R.string.hello_world))
+                .extend(new NotificationCompat.WearableExtender().addAction(action))
                 .build();
-        mGoogleApiClient.connect();
+
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(Wearable.API)
+//                .build();
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(001, notification);
 
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                if(mGoogleApiClient == null)
-                    return;
+//                if(mGoogleApiClient == null)
+//                    return;
 
 
             }
