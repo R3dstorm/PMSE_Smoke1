4f155c22dc798cdc69a17cc8218fff7d7a454bc4
SteffenHolz <steffen@holzhauer-freiburg.de>
2019-01-11 21:38:19 +0100

    vibration added to onCreate function

--- wear/src/main/java/de/uni_freiburg/iems/beatit/SmokeDetectedPopUpActivity.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/SmokeDetectedPopUpActivity.java
@@ -1,6 +1,8 @@
 package de.uni_freiburg.iems.beatit;
 
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
 
 public class SmokeDetectedPopUpActivity extends WearableActivity {
@@ -9,6 +11,7 @@ public class SmokeDetectedPopUpActivity extends WearableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smoking_detected);
+        ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
 
     }
 }
