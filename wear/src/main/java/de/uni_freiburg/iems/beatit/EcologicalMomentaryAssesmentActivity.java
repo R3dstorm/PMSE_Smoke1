775e1d297358d34c1fa9029a1e4952792e44c40a
Christoph <warri1@gmx.net>
2019-01-18 00:50:52 +0100

    SMOK-34 #time 2h - Added features to display results of AI-Model

--- wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
@@ -5,20 +5,26 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import MachineLearningModule.ModelHandler;
 import sensorReadoutModule.dataAcquisitionActivity;
 import sensorReadoutModule.SensorReadoutService;
 
-public class EcologicalMomentaryAssesmentActivity extends WearableActivity implements View.OnClickListener {
+/* Callback interface for measurementCompleteEvent*/
+interface ModelEvaluatedListener {
+    void modelEvaluatedCB(boolean smoking);
+}
+
+public class EcologicalMomentaryAssesmentActivity extends WearableActivity implements View.OnClickListener, ModelEvaluatedListener {
 
     private TextView mTextView;
     private ToggleButton startButton;
     private Intent sensorServiceIntent;
     private Boolean sensorServiceStarted = false;
     private Mediator sensorAiMediator;
+    private CheckBox smokingDetected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,7 @@ public class EcologicalMomentaryAssesmentActivity extends WearableActivity imple
         setAmbientEnabled();
         Button daqButton = findViewById(R.id.startPageButtonLable);
         startButton = findViewById(R.id.startPageButtonPlay);
+        smokingDetected = findViewById(R.id.checkBox);
         daqButton.setOnClickListener(this);
     }
     @Override
@@ -64,12 +71,23 @@ public class EcologicalMomentaryAssesmentActivity extends WearableActivity imple
             startService(sensorServiceIntent);
             sensorServiceStarted = true;
 
-            sensorAiMediator = new Mediator(this);
+            sensorAiMediator = new Mediator(this, EcologicalMomentaryAssesmentActivity.this);
         }
         else
         {
             stopService(sensorServiceIntent);
             sensorServiceStarted = false;
+        }
+    }
+
+    @Override
+    public void modelEvaluatedCB(boolean smoking) {
+        /* The measurement is completed*/
+        if (smoking){
+            smokingDetected.setChecked(true);
+        }
+        else{
+            smokingDetected.setChecked(false);
         }
     }
 }
--- wear/src/main/java/de/uni_freiburg/iems/beatit/Mediator.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/Mediator.java
@@ -4,18 +4,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 import MachineLearningModule.ModelHandler;
 import sensorReadoutModule.SensorReadoutService;
 import sensorReadoutModule.SensorReadoutService.SensorReadoutBinder;
 
 public class Mediator {
-
-    private Timer timer;
 
     final private int SLIDING_STEP_MS = 1000;
     final private int START_DELAY_MS = 100;
@@ -25,6 +21,9 @@ public class Mediator {
     private boolean sensorServiceBound = false;
     private boolean sensorServiceRunning = false;
     private ModelHandler m;
+    private ModelEvaluatedListener modelEvaluatedListener;
+    private Context myContext;
+    private Handler mainHandler;
 
     private ServiceConnection sensorServiceConnection = new ServiceConnection() {
 
@@ -44,7 +43,9 @@ public class Mediator {
         }
     };
 
-    Mediator (Context context){
+    Mediator(Context context, ModelEvaluatedListener _modelEvaluatedListener) {
+        myContext = context;
+        mainHandler = new Handler(myContext.getMainLooper());
         /* Start ModelCalcCycleTimerTask */
         startModelCalcCycleTimerTask(START_DELAY_MS, SLIDING_STEP_MS);
         /* Binding to Service*/
@@ -53,34 +54,36 @@ public class Mediator {
             sensorServiceIntent.putExtra("BIND_SENSOR_SERVICE", true); /* TODO is this necessary? */
             context.bindService(sensorServiceIntent, sensorServiceConnection, Context.BIND_AUTO_CREATE);
         }
-
+        modelEvaluatedListener = _modelEvaluatedListener;
         m = new ModelHandler();
         m.loadModel(context.getAssets());
     }
 
-    private void startModelCalcCycleTimerTask(int startDelay, int repeatDelay) {
-        timer = new Timer();
-        TimerTask modelCalcCycle = new TimerTask() {
-            @Override
-            public void run() {
-                /* Collect Data from Sensor */
-                if (sensorServiceBound){
-                    sensorServiceRunning = sensorService.isSensorServiceRunning();
-                }
-                if (sensorServiceRunning){
-                    /* Start continuous measurement */
-                    sensorService.triggerContinuousMeasurement();
-
-                    /* Try to read out and process data*/
-                    if (sensorService.isContMeasDataAvailable()){
-                        /* Hand continuous data to ML-Module*/
-                        m.predict(sensorService.getContinuousMeasurementDataStorage());
-                    }
-                    /* TODO Connect to GUI?*/
-                }
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            /* Collect Data from Sensor */
+            if (sensorServiceBound) {
+                sensorServiceRunning = sensorService.isSensorServiceRunning();
             }
-        };
-        timer.scheduleAtFixedRate(modelCalcCycle, startDelay, repeatDelay);
+            if (sensorServiceRunning) {
+                /* Start continuous measurement */
+                sensorService.triggerContinuousMeasurement();
 
+                /* Try to read out and process data*/
+                if (sensorService.isContMeasDataAvailable()) {
+                    /* Hand continuous data to ML-Module*/
+                    boolean smokingLabel = m.predict(sensorService.getContinuousMeasurementDataStorage());
+                    modelEvaluatedListener.modelEvaluatedCB(smokingLabel);
+                }
+                /* TODO Connect to GUI?*/
+            }
+            mainHandler.postDelayed(runnable, SLIDING_STEP_MS);
+        }
+    };
+
+
+    private void startModelCalcCycleTimerTask(int startDelay, int repeatDelay) {
+        mainHandler.postDelayed(runnable, repeatDelay);
     }
 }
