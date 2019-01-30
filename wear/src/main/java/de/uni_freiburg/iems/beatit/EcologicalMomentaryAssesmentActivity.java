40cef1b639a772016ff441ff488611e40c9494fc
rkreitner <sunstone@t-online.de>
2019-01-30 18:00:27 +0100

    fastfix

--- wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/EcologicalMomentaryAssesmentActivity.java
@@ -3,6 +3,7 @@ package de.uni_freiburg.iems.beatit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
