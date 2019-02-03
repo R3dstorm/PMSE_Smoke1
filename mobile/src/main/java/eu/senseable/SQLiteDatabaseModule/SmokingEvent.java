37d1ce81a5d7408e052b0d1f19ee0a0133309fa0
SteffenHolz <steffen@holzhauer-freiburg.de>
2019-02-03 12:48:27 +0100

    added files to repo

---
+++ mobile/src/main/java/eu/senseable/SQLiteDatabaseModule/SmokingEvent.java
@@ -0,0 +1,57 @@
+package eu.senseable.SQLiteDatabaseModule;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "smoking_event_table")
+public class SmokingEvent {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mTest;
+
+    @NonNull
+    @ColumnInfo(name = "Start_Date")
+    private String startDate;
+
+    @NonNull
+    @ColumnInfo(name = "Start_Time")
+    private String startTime;
+
+    @NonNull
+    @ColumnInfo(name = "Stop_Date")
+    private String stopDate;
+
+    @NonNull
+    @ColumnInfo(name = "Stop_Time")
+    private String stopTime;
+
+    @NonNull
+    @ColumnInfo(name = "Event_Confirmed")
+    private boolean eventConfirmed;
+
+    public SmokingEvent(@NonNull String test, @NonNull String startDate, @NonNull String startTime,
+                        @NonNull String stopDate, @NonNull String stopTime,
+                        @NonNull boolean eventConfirmed) {
+        this.mTest = test;
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.stopDate = stopDate;
+        this.stopTime = stopTime;
+        this.eventConfirmed = eventConfirmed;
+    }
+
+    public String getMTest(){return mTest;}
+    public int getId(){return id;}
+    public void setId(int id) {this.id = id;}
+    public String getStartDate(){return startDate;}
+    public String getStartTime(){return startTime;}
+    public String getStopDate(){return stopDate;}
+    public String getStopTime(){return stopTime;}
+    public boolean getEventConfirmed(){return eventConfirmed;}
+}
---
+++ mobile/src/main/java/eu/senseable/SQLiteDatabaseModule/SmokingEventListAdapter.java
@@ -0,0 +1,63 @@
+/* Credits: Major parts of sources from https://codelabs.developers.google.com/codelabs/android-room-with-a-view */
+
+package eu.senseable.SQLiteDatabaseModule;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import eu.senseable.cigarettetracker.R;
+
+public class SmokingEventListAdapter extends RecyclerView.Adapter<SmokingEventListAdapter.SmokingEventViewHolder> {
+
+    class SmokingEventViewHolder extends RecyclerView.ViewHolder {
+        private final TextView smokingEventItemView;
+
+        private SmokingEventViewHolder(View itemView) {
+            super(itemView);
+            smokingEventItemView = itemView.findViewById(R.id.my_recycler_view);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<SmokingEvent> mSmokingEvents; // Cached copy of smoking events
+
+    public SmokingEventListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+    @Override
+    public SmokingEventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.activity_counter, parent, false);
+        return new SmokingEventViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SmokingEventViewHolder holder, int position) {
+        if (mSmokingEvents != null) {
+            SmokingEvent current = mSmokingEvents.get(position);
+            /* Write display data to view */
+            holder.smokingEventItemView.setText(current.getStartTime());
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.smokingEventItemView.setText("No Smoke Event");
+        }
+    }
+
+    public void setEvents(List<SmokingEvent> events){
+        mSmokingEvents = events;
+        notifyDataSetChanged();
+    }
+
+    // getItemCount() is called many times, and when it is first called,
+    // mWords has not been updated (means initially, it's null, and we can't return null).
+    @Override
+    public int getItemCount() {
+        if (mSmokingEvents != null)
+            return mSmokingEvents.size();
+        else return 0;
+    }
+}
