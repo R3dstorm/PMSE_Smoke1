4ae557746831550efb3f5211054f380dd5e48c5c
Christoph <warri1@gmx.net>
2019-02-14 21:37:42 +0100

    SMOK-57 #time 1h added check for nearby devices with error display if none is around

--- wear/src/main/java/de/uni_freiburg/iems/beatit/SynchronizeService.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/SynchronizeService.java
@@ -9,6 +9,11 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.commondataobjects.SmokingEventDTO;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.CapabilityClient;
+import com.google.android.gms.wearable.CapabilityInfo;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -16,6 +21,8 @@ import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import SQLiteDatabaseModule.SmokingEvent;
 import SQLiteDatabaseModule.SmokingEventRepository;
@@ -27,10 +34,12 @@ public class SynchronizeService extends JobIntentService{
     static final int JOB_ID = 1000;
     final Handler mHandler = new Handler();
     private SmokingEventRepository smEvRepo;
-
     int lastSyncLabelId = 0;                /* Holds the id of the latest sync label*/
     int latestEventId = 0;
     private Synchronize dBsyncHandler;      /* Sync handler for data base */
+
+    boolean nodeIsNearby = false;
+    private static final String CAPABILITY_NAME = "beat_smoking";
     /**
      * Convenience method for enqueuing work in to this service.
      */
@@ -50,25 +59,30 @@ public class SynchronizeService extends JobIntentService{
 
         if (intent.getBooleanExtra("SEND_NEW_EVENTS",false) == true) {
             Log.i("SynchronizeService", "Executing work: " + intent);
-            String label = intent.getStringExtra("label");
-            if (label == null) {
-                label = intent.toString();
-            }
-            toast("Sync Starting");
 
-            /* Search data base for the sync label and send all later events to phone:*/
-            /* Find Sync label*/
-            newSmokingEvents = smEvRepo.getLatestSyncLabelIdTest(); /* TODO rename repo-method */
-            if (!newSmokingEvents.isEmpty()) {
-                /* SyncLabel available -> only get not synchronized events */
-                lastSyncLabelId = newSmokingEvents.get(0).getId();
-                newSmokingEvents = smEvRepo.getNewSyncEventsTest(lastSyncLabelId);
-            } else {
-                /* there has been no SyncLabel set yet -> get all events */
-                newSmokingEvents = smEvRepo.getAllEventsList();
+            /* find a connected device: */
+            nodeIsNearby = nodeNearby();
+
+            if (nodeIsNearby) {
+                toast("Sync Starting");
+
+                /* Search data base for the sync label and send all later events to phone:*/
+                /* Find Sync label*/
+                newSmokingEvents = smEvRepo.getLatestSyncLabelIdTest(); /* TODO rename repo-method */
+                if (!newSmokingEvents.isEmpty()) {
+                    /* SyncLabel available -> only get not synchronized events */
+                    lastSyncLabelId = newSmokingEvents.get(0).getId();
+                    newSmokingEvents = smEvRepo.getNewSyncEventsTest(lastSyncLabelId);
+                } else {
+                    /* there has been no SyncLabel set yet -> get all events */
+                    newSmokingEvents = smEvRepo.getAllEventsList();
+                }
+                /* Send data to phone */
+                dBsyncHandler.sendSyncMessage(newSmokingEvents);
             }
-            /* Send data to phone */
-            dBsyncHandler.sendSyncMessage(newSmokingEvents);
+            else{
+                toast("No device to sync available ");
+            }
         }
         else if (intent.getBooleanExtra("NEW_EVENTS_RECEIVED",false) == true){
 
@@ -97,7 +111,9 @@ public class SynchronizeService extends JobIntentService{
     @Override
     public void onDestroy() {
         super.onDestroy();
-        toast("Sync Finished");
+        if(nodeIsNearby) {
+            toast("Sync Finished");
+        }
     }
 
     // Helper for showing tests
@@ -107,6 +123,29 @@ public class SynchronizeService extends JobIntentService{
                 Toast.makeText(SynchronizeService.this, text, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private boolean nodeNearby() {
+        boolean result = false;
+        CapabilityInfo capabilityInfo = null;
+
+        try {
+            capabilityInfo = Tasks.await(
+                    Wearable.getCapabilityClient(getApplicationContext()).getCapability(
+                            CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Set<Node> connectedNodes = capabilityInfo.getNodes();
+        for (Node node : connectedNodes) {
+            if (node.isNearby()) {
+                result = true;
+            }
+        }
+        return result;
     }
 
     List<SmokingEvent> deserializeEvents(byte[] data){
