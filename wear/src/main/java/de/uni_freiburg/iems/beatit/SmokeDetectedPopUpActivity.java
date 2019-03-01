5c37e313f0c09d8f9ff10d261b7982ac7d493119
SteffenHolz <steffen@holzhauer-freiburg.de>
2019-03-01 19:50:34 +0100

    pop-up beeenden nach erkanntem raucherevent

--- wear/src/main/java/de/uni_freiburg/iems/beatit/SmokeDetectedPopUpActivity.java
+++ wear/src/main/java/de/uni_freiburg/iems/beatit/SmokeDetectedPopUpActivity.java
@@ -1,14 +1,11 @@
 package de.uni_freiburg.iems.beatit;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
-
-import java.lang.reflect.Array;
 
 public class SmokeDetectedPopUpActivity extends WearableActivity {
 
@@ -18,6 +15,16 @@ public class SmokeDetectedPopUpActivity extends WearableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smoking_detected);
         ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                onDestroy();
+            }
+        }.start();
     }
 
 
