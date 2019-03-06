16e6fe25f08a9a707f364ba3c06532b4f46910a2
rkreitner <sunstone@t-online.de>
2019-03-06 20:30:07 +0100

    update desktop scripts

--- scripts/sensors2features.py
+++ scripts/sensors2features.py
@@ -11,9 +11,9 @@ data = content[:,1:7]
 labels = content[:,0]
 
 sliding_seconds = 1
-window_seconds = 30
+window_seconds = 20
 data_frequency = 50
-feature_count  = 12
+feature_count  = 24 # 6 x mean, 6 x std, 6 x min, 6 x max
 
 sliding_size = sliding_seconds * data_frequency
 window_size = window_seconds * data_frequency
@@ -25,6 +25,8 @@ if data.shape[1] != 6:
 
 mean = np.zeros(6)
 std = np.zeros(6)
+amin = np.zeros(6)
+amax = np.zeros(6)
 size = data.shape[0] // sliding_size
 features = np.zeros([size, feature_count + 1])
 new_labels = np.zeros([size])
@@ -39,10 +41,14 @@ while offset + window_size <= data.shape[0]:
   for column in range(0, 6):
     mean[column] = np.mean(data[offset:offset + window_size,column])
     std[column] = np.std(data[offset:offset + window_size,column])
+    amin[column] = np.min(data[offset:offset + window_size,column])
+    amax[column] = np.max(data[offset:offset + window_size,column])
 
   features[row] = [new_label,
                    mean[0], mean[1], mean[2], mean[3], mean[4], mean[5],
-                   std[0], std[1], std[2], std[3], std[4], std[5]]
+                   std[0], std[1], std[2], std[3], std[4], std[5],
+                   amin[0], amin[1], amin[2], amin[3], amin[4], amin[5],
+                   amax[0], amax[1], amax[2], amax[3], amax[4], amax[5]]
 
   offset += sliding_size
   row += 1
